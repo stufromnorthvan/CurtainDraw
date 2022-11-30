9c283da9818733fc582e1ae57a08a1c185449918
stufromnorthvan <stumitchell@irsss.ca>
2022-11-29 20:14:53 -0800

    lint

--- express-back-end/db/queries/profile.js
+++ express-back-end/db/queries/profile.js
@@ -3,16 +3,17 @@ const db = require('../connection');
 const getUsers = () => {
   return db.query('SELECT * FROM users;')
     .then(data => {
-      console.log("Get User From Id DB Query Completed: ", data.rows)
+      console.log("Get User From Id DB Query Completed: ", data.rows);
       return data.rows;
     });
 };
+
 const getUserById = (userId) => {
   return db.query(`SELECT * FROM users
   WHERE id = ${userId} ;
   `)
     .then(data => {
-      console.log("Get User From Id DB Query Completed: ",data.rows);
+      console.log("Get User From Id DB Query Completed: ", data.rows);
       return data.rows;
     });
 };
@@ -24,11 +25,11 @@ const getDrawingsByUserId = (userId) => {
   JOIN users on user_id = users.id
   WHERE user_id = ${userId}
   RETURNING *`)
-  .then(data => {
-    console.log("Drawings by User ID DB Query Completed: ",data.rows)
-    return data.rows;
-  });
-}
+    .then(data => {
+      console.log("Drawings by User ID DB Query Completed: ", data.rows);
+      return data.rows;
+    });
+};
 
 const getTotalDrawings = (userId) => {
   return db.query(`SELECT count(drawings.*) as total_drawings
@@ -36,8 +37,8 @@ const getTotalDrawings = (userId) => {
   JOIN users on user_id = users.id
   WHERE user_id = ${userId}
   RETURNING *`)
-  .then(data => {
-    console.log("Total Drawings Count DB Query Completed: ",data.rows)
-    return data.rows;
-  });
-}
+    .then(data => {
+      console.log("Total Drawings Count DB Query Completed: ", data.rows);
+      return data.rows;
+    });
+};
